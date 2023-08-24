6e37e59e4e4a68c9995f53aa05851aab387b1be3
Calle1122 <carl@lindstedt.net>
2023-08-24 11:03:25 +0200

    Updated Project Files

--- Assets/Scripts/Game/FirstName_LastName/Team_FirstName_LastName.cs
+++ Assets/Scripts/Game/FirstName_LastName/Team_FirstName_LastName.cs
@@ -1,4 +1,5 @@
 using Game;
+using Graphs;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Scripts/Game/FirstName_LastName/Unit_FirstName_LastName.cs
+++ Assets/Scripts/Game/FirstName_LastName/Unit_FirstName_LastName.cs
@@ -1,4 +1,6 @@
+using AI;
 using Game;
+using Graphs;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +9,12 @@ namespace FirstName_LastName
 {
     public class Unit_FirstName_LastName : Unit
     {
+        #region Properties
+
+        public new Team_FirstName_LastName Team => base.Team as Team_FirstName_LastName;
+
+        #endregion
+
         protected override Unit SelectTarget(List<Unit> enemiesInRange)
         {
             return enemiesInRange[Random.Range(0, enemiesInRange.Count)];
