eedbe3fc66c0258735ab6c5c4b3fa091a322387d
Calle1122 <carl@lindstedt.net>
2023-08-24 12:25:03 +0200

    More setup

--- Assets/Scripts/Game/Carl_Lindstedt/Unit_Carl_Lindstedt.cs
+++ Assets/Scripts/Game/Carl_Lindstedt/Unit_Carl_Lindstedt.cs
@@ -17,7 +17,30 @@ namespace Carl_Lindstedt
 
         protected override Unit SelectTarget(List<Unit> enemiesInRange)
         {
-            return enemiesInRange[Random.Range(0, enemiesInRange.Count)];
+            // pick a random target!
+            return enemiesInRange != null && enemiesInRange.Count > 0 ? 
+                enemiesInRange[Random.Range(0, enemiesInRange.Count)] : null;
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            StartCoroutine(StupidLogic());
+        }
+
+        IEnumerator StupidLogic()
+        {
+            while (true)
+            {
+                // wait (or take cover)
+                TargetNode = null;
+                yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+
+                // move randomly
+                TargetNode = Battlefield.Instance.GetRandomNode();
+                yield return new WaitForSeconds(Random.Range(4.0f, 6.0f));
+            }
         }
     }
 }
