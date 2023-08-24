b4db491dfde521ca7bb422710f8554f2da37a7fc
Calle1122 <carl@lindstedt.net>
2023-08-24 20:29:54 +0200

    Formations

--- Assets/Scripts/Game/Carl_Lindstedt/Team_Carl_Lindstedt.cs
+++ Assets/Scripts/Game/Carl_Lindstedt/Team_Carl_Lindstedt.cs
@@ -1,7 +1,9 @@
+using System;
 using Game;
 using Graphs;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Carl_Lindstedt
@@ -11,12 +13,154 @@ namespace Carl_Lindstedt
         [SerializeField]
         private Color   m_myFancyColor;
 
+        public GameObject squadLeaderTargetPoint;
+        
+        public List<GameObject> targetPoints;
+        
+        public bool formationFollow = true;
+        
+        private List<Unit> friendlyUnits;
+        private int lastFrameAliveFriendlies;
+        
         #region Properties
 
         public override Color Color => m_myFancyColor;
 
         #endregion
 
+        protected override void Start()
+        {
+            base.Start();
+
+            //Gets all friendly units
+            friendlyUnits = GetFriendlyTeamList();
+            
+            //Set the units formation numbers to their new positions
+            for (int i = 0; i < friendlyUnits.Count; i++)
+            {
+                Unit_Carl_Lindstedt myUnit = friendlyUnits[i] as Unit_Carl_Lindstedt;
+                myUnit.FormationNumber = i;
+            }
+        }
+
+        private void Update()
+        {
+            if (Units.Count() != lastFrameAliveFriendlies)
+            {
+                friendlyUnits = GetFriendlyTeamList();
+                ShuffleFormation();
+            }
+            else
+            {
+                //Have the main formation point follow the squad leader (if bool true)
+                if (formationFollow)
+                {
+                    targetPoints[0].transform.position = friendlyUnits[0].transform.position;
+                }
+            }
+            
+            //Get alive enemies & friendlies
+            List<Unit> enemyUnits = GetEnemyTeamList();
+            
+            //Rotate formation points towards the enemy average position
+            Vector3 enemyAveragePosition = GetAveragePosition(enemyUnits);
+            targetPoints[0].transform.LookAt(enemyAveragePosition, transform.up);
+            
+            //Update the squad leaders target position
+            List<Unit> allAliveUnits = new List<Unit>();
+            allAliveUnits.AddRange(friendlyUnits);
+            allAliveUnits.AddRange(enemyUnits);
+            squadLeaderTargetPoint.transform.position = GetAveragePosition(allAliveUnits);
+            
+            //Set last known number of alive friendlies
+            lastFrameAliveFriendlies = friendlyUnits.Count;
+        }
+
+        public List<Unit> GetFriendlyTeamList()
+        {
+            List<Unit> newFriendlyUnits = new List<Unit>(Units);
+            return newFriendlyUnits;
+        }
+        
+        public List<Unit> GetEnemyTeamList()
+        {
+            List<Unit> enemyUnits = new List<Unit>(EnemyTeam.Units);
+            return enemyUnits;
+        }
+        
+        public void ShuffleFormation()
+        {
+            StartCoroutine(FreezeFormation());
+            
+            List<Unit> newUnitFormation = new List<Unit>();
+            //Shuffle the list
+            switch (friendlyUnits.Count)
+            {
+                case 5:
+                    newUnitFormation.Add(friendlyUnits[1]);
+                    newUnitFormation.Add(friendlyUnits[2]);
+                    newUnitFormation.Add(friendlyUnits[3]);
+                    newUnitFormation.Add(friendlyUnits[4]);
+                    newUnitFormation.Add(friendlyUnits[0]);
+                    break;
+                
+                case 4:
+                    newUnitFormation.Add(friendlyUnits[1]);
+                    newUnitFormation.Add(friendlyUnits[2]);
+                    newUnitFormation.Add(friendlyUnits[3]);
+                    newUnitFormation.Add(friendlyUnits[0]);
+                    break;
+                
+                case 3:
+                    newUnitFormation.Add(friendlyUnits[1]);
+                    newUnitFormation.Add(friendlyUnits[2]);
+                    newUnitFormation.Add(friendlyUnits[0]);
+                    break;
+                
+                case 2:
+                    newUnitFormation.Add(friendlyUnits[1]);
+                    newUnitFormation.Add(friendlyUnits[0]);
+                    break;
+                
+                case 1:
+                    newUnitFormation.Add(friendlyUnits[0]);
+                    break;
+            }
+
+            friendlyUnits = newUnitFormation;
+
+            //Set the units formation numbers to their new positions
+            for (int i = 0; i < friendlyUnits.Count; i++)
+            {
+                Unit_Carl_Lindstedt myUnit = friendlyUnits[i] as Unit_Carl_Lindstedt;
+                myUnit.FormationNumber = i;
+            }
+        }
+        
+        IEnumerator FreezeFormation()
+        {
+            formationFollow = false;
+            yield return new WaitForSeconds(5f);
+            formationFollow = true;
+        }
+
+        public Vector3 GetAveragePosition(List<Unit> unitsToAverage)
+        {
+            if(unitsToAverage.Count == 0)
+            {
+                return Vector3.zero;
+            }
+ 
+            Vector3 averageVector = Vector3.zero;
+ 
+            foreach(Unit unit in unitsToAverage)
+            {
+                averageVector += unit.transform.position;
+            }
+ 
+            return (averageVector / unitsToAverage.Count);
+        }
+        
         public GraphUtils.Path GetShortestPath(Battlefield.Node start, Battlefield.Node goal)
         {
             if (start == null ||
--- Assets/Scripts/Game/Carl_Lindstedt/Unit_Carl_Lindstedt.cs
+++ Assets/Scripts/Game/Carl_Lindstedt/Unit_Carl_Lindstedt.cs
@@ -9,12 +9,22 @@ namespace Carl_Lindstedt
 {
     public class Unit_Carl_Lindstedt : Unit
     {
-        public GameObject targetPointer;
+        private int m_formationNumber;
         
         #region Properties
 
         public new Team_Carl_Lindstedt Team => base.Team as Team_Carl_Lindstedt;
 
+        public int FormationNumber
+        {
+            get => m_formationNumber;
+
+            set
+            {
+                m_formationNumber = value;
+            }
+        }
+        
         #endregion
 
         protected override Unit SelectTarget(List<Unit> enemiesInRange)
@@ -24,6 +34,11 @@ namespace Carl_Lindstedt
                 enemiesInRange[Random.Range(0, enemiesInRange.Count)] : null;
         }
 
+        protected override GraphUtils.Path GetPathToTarget()
+        {
+            return Team.GetShortestPath(CurrentNode, TargetNode);
+        }
+
         protected override void Start()
         {
             base.Start();
@@ -31,58 +46,23 @@ namespace Carl_Lindstedt
             StartCoroutine(PathfindingLogic());
         }
 
-        protected override void Update()
-        {
-            base.Update();
-
-            if (targetPointer != null)
-            {
-                List<Unit> enemyUnits = new List<Unit>(Team.EnemyTeam.Units);
-                List<Unit> myUnits = new List<Unit>(Team.Units);
-
-                foreach (var unit in myUnits)
-                {
-                    enemyUnits.Add(unit);
-                }
-                targetPointer.transform.position = GetAveragePosition(enemyUnits);
-            }
-        }
-
         IEnumerator PathfindingLogic()
         {
             while (true)
             {
-                TargetNode = null;
-                yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
-                
-                List<Unit> enemyUnits = new List<Unit>(Team.EnemyTeam.Units);
-                List<Unit> myUnits = new List<Unit>(Team.Units);
-
-                foreach (var unit in myUnits)
+                //if you are the squad leader... pathfind towards target
+                if (FormationNumber == 0 && Team.formationFollow)
                 {
-                    enemyUnits.Add(unit);
+                    TargetNode = GraphUtils.GetClosestNode<Battlefield.Node>(Battlefield.Instance, Team.squadLeaderTargetPoint.transform.position);
+                    yield return new WaitForSeconds(.1f);
                 }
-                
-                TargetNode = GraphUtils.GetClosestNode<Battlefield.Node>(Battlefield.Instance, GetAveragePosition(enemyUnits));
-                yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+
+                else
+                {
+                    TargetNode = GraphUtils.GetClosestNode<Battlefield.Node>(Battlefield.Instance, Team.targetPoints[FormationNumber].transform.position);
+                    yield return new WaitForSeconds(.1f);
+                }
             }
-        }
-        
-        private Vector3 GetAveragePosition(List<Unit> unitsToAverage)
-        {
-            if(unitsToAverage.Count == 0)
-            {
-                return Vector3.zero;
-            }
- 
-            Vector3 averageVector = Vector3.zero;
- 
-            foreach(Unit unit in unitsToAverage)
-            {
-                averageVector += unit.transform.position;
-            }
- 
-            return (averageVector / unitsToAverage.Count);
         }
     }
 }
